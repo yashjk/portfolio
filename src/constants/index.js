cfda4f1eed921a99997b975be907121aa95d4f35
Yash Joshi <injose.joshi@gmail.com>
2024-06-26 22:16:57 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -179,7 +179,7 @@ export const projects = [
 		name: "Full Stack Instagram Clone",
 		description:
 			"Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-		link: "https://github.com/adrianhajdin/social_media_app",
+		link: "https://github.com/yashjk/insta_clone",
 	},
 	{
 		iconUrl: estate,
