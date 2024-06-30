56dcc0e7b45fc09c4d014aebfa47ad8fa003716c
Yash Joshi <injose.joshi@gmail.com>
2024-06-30 13:45:15 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -184,7 +184,7 @@ export const projects = [
 		name: "Full Stack Instagram Clone",
 		description:
 			"Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.",
-		link: "https://github.com/yashjk/insta_clone",
+		link: "https://github.com/yashjk/SocialSphere",
 	},
 	{
 		iconUrl: estate,
